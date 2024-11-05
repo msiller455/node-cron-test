84e430130e6ca9a7db901e5160af1ff1bd81a6c3
jsnhn <jayhh122@gmail.com>
2024-11-04 16:23:43 -0800

    sets up daily and weekly cron jobs.

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+const cron = require('node-cron');
+require('dotenv').config()
+
+// cron syntax to perform at 7AM everyday.
+cron.schedule('0 7 * * *', () => {
+    console.log('daily running')
+});
+
+// cron syntax to perform at 7AM every Tues.
+cron.schedule('0 7 * * Tues', () => {
+    console.log('weekly running')
+});
+
+
+
+// cron.schedule('*/5 * * * * *', () => {
+//     console.log('running every 5 seconds')
+// });
+
+// cron.schedule('*/15 * * * * *', () => {
+//     console.log('running every 15 seconds')
+// })
